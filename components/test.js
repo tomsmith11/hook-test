9b126be685ae02eee767e4732bbdcd100fb35095
Tom Smith <tom@craftscale.uk>
2024-12-12 16:59:55 +0000

    commit

---
+++ components/test.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function Test() {
+  const [count, setCount] = useState(0);
+
+  const textColour = count < 0 ? "text-red-500" : "text-green-500";
+
+  console.log("Test component render begun, count is", count);
+
+  useEffect(() => {});
+
+  return (
+    <main className="flex flex-col items-center p-24">
+      <p className={`p-2 text-8xl font-bold ${textColour}`}>{count}</p>
+      <div className="flex flex-row">
+        <button
+          className="border border-gray-800 p-6 m-6"
+          onClick={() => setCount(count + 1)}
+        >
+          increment
+        </button>
+
+        <button
+          className="border border-gray-800 p-6 m-6"
+          onClick={() => setCount(count - 1)}
+        >
+          decrement
+        </button>
+      </div>
+    </main>
+  );
+}
